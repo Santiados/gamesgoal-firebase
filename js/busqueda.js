d54ede49bd656da3c8356a1f9483be2b9c4fc318
phathatha47 <santireven20@gmail.com>
2018-03-22 23:23:28 +0100

    update ajax process

--- js/busqueda.js
+++ js/busqueda.js
@@ -3,40 +3,51 @@ entrada.value = '';
 var resultado = document.getElementsByClassName('objetos')[0];
 window.addEventListener('load', iniciar );
 
-  function iniciar(){
-    entrada.addEventListener("keyup",mostrar);
-  }
+function iniciar(){
+  entrada.addEventListener("keyup",mostrar);
+}
 
-  function mostrar(e){
-    var valorPeticion = e.target.value.trim();
-    if(valorPeticion == ''){
-      resultado.innerHTML = '';
-      return;
-    }else{
-      try {
-        fetch('js/productos.json/productos?id_like=' + valorPeticion)
-        .then(response => response.json())
-        .then(json => mostrar(json))
-        .catch(error => resultado.innerHTML = 'No es posible conectarse')
+function mostrar(e){
+  var valorPeticion = e.target.value.trim();
+  if(valorPeticion == ''){
+    resultado.innerHTML = '';
+    return;
+  }else{
+    try {
+      fetch('js/productos.json')
+      .then(response => response.json())
+      .then(json => procesarDatos(json.productos))
+      .catch(error => resultado.innerHTML = 'No es posible conectarse')
         //peticion del archivo JSON y se pasa a una función para su procesamiento
         
+        function mostrarResultado(res){
+          console.log(res)
+          if(res.length == 0){
+            resultado.innerHTML = 'No hay resultados';
+          }else {
+            resultado.innerHTML = '';
+            res.forEach( function(objeto, index) {
+              $('div.objetos').append('<div class="producto card">'
+                + '<p>' + objeto.id + '</p>'
+                + '<span>' + objeto.Capacidad + '</span>'
+                + '<span>' + objeto.Ram + '</span>'
+                + '<span>' + objeto.Precio + '</span>'
+                + "<button class='redirigir btn btn-danger' >Mas</button>");
+            });
+          }
+        }
 
-        function mostrar(respuesta){
+        function procesarDatos(respuesta){
           console.log(respuesta)
-            if(respuesta.length>0){
-              resultado.innerHTML = '';
-                for (var i = 0; i < respuesta.length; i++) {
-                  var objeto = respuesta[i];
-                  $('div.objetos').append('<div class="producto card">'
-                                        + '<p>' + objeto.id + '</p>'
-                                        + '<span>' + objeto.Capacidad + '</span>'
-                                        + '<span>' + objeto.Ram + '</span>'
-                                        + '<span>' + objeto.Precio + '</span>'
-                                        + "<button class='redirigir btn btn-danger' >Mas</button>");
-                }            
-            }else {
-              resultado.innerHTML = 'No hay nada relacionado';
-            }
+          if(respuesta.length>0){
+            var aux = [];
+            respuesta.forEach( function(objeto, index) {
+              if(objeto.id.toLowerCase().includes(valorPeticion.toLowerCase())){
+                aux.push(objeto);
+              }
+            });
+            mostrarResultado(aux);          
+          }
         }
       } catch(e) {
         // statements
